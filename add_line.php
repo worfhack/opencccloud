b4da43cb2b03822c7b2eb2ec1625bae1d7d07dcb
worf <tdc@wizzmedia.fr>
2021-01-26 01:40:11 +0100

    fix namle

---
+++ add_line.php
@@ -0,0 +1,21 @@
+<?php
+$new_array = [];
+$array = explode("\n", file_get_contents('wp-config.php'));
+$id = $argv[1];
+$key = $argv[2];
+
+foreach ($array as $line){
+
+    $new_array[] = $line;
+    if ($line == "<?php"){
+        $text = "define( 'AS3CF_SETTINGS', serialize( array('provider' => 'aws','access-key-id' =>'".$id."' ,'secret-access-key' => '".$key."',) ) );";
+        $new_array[] = $text;
+    }
+}
+$file="wp-config.php";
+file_put_contents($file, "");
+foreach($new_array as $line){
+    file_put_contents($file, $line . "\n", FILE_APPEND );
+
+}
+?>
